0a56aecc0b23f13316e7ffa68bf2defb7503d8ad
zoomjuice <itsjunkmaill@gmail.com>
2021-02-06 10:21:37 -0500

    Expand notes in zip lesson

--- Lesson 4 - Lists/4_04-Zip.py
+++ Lesson 4 - Lists/4_04-Zip.py
@@ -11,4 +11,14 @@ list_of_names_and_dogs_names = list(names_and_dogs_names)
 
 print(list_of_names_and_dogs_names)
 
-list_of_names_and_dogs_names[0].append('foo')
+# Trying to modify a tuple throws an error
+# list_of_names_and_dogs_names[0][1] = 'Spot'
+
+# To modify an element in a zipped list, convert the tuples in it to lists
+for i in range(len(list_of_names_and_dogs_names)):
+    list_of_names_and_dogs_names[i] = list(list_of_names_and_dogs_names[i])
+
+# After conversion, elements may be changed
+list_of_names_and_dogs_names[0][1] = 'Spot'
+
+print(list_of_names_and_dogs_names)
